caf38b15b863a1f164d8cbd896f7f482821d3325
cqhung1412 <42210002+cqhung1412@users.noreply.github.com>
2021-02-01 20:16:33 +0700

    Revert "setup"
    This reverts commit 2ed6615f607706bf6ca28bf4a6065ce6bec3e276.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import classes from './App.css';
-
-import Layout from './hoc/Layout/Layout';
-import AuxWithClass from './hoc/Auxiliary/AuxWithClass/AuxWithClass';
-import Course from './containers/Course/Course';
-import Home from './containers/Home/Home';
-import Report from './containers/Report/Report';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Layout>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/course' component={Course} />
-        <Route path='/report' component={Report} />
-        <Route render={() => <h1>404 Not Found!!!</h1>} />
-      </Switch>
-    </Layout>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
-export default AuxWithClass(App, classes.App);
+export default App;
--- src/axios-orders.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const instance = axios.create({
-  baseURL: 'https://bear-burger-builder.firebaseio.com/'
-})
-
-export default instance
