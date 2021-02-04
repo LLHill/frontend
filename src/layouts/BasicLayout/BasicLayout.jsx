2026b1ddb59f63aeac92a3ccc1ed680e2ed102e7
cqhung1412 <42210002+cqhung1412@users.noreply.github.com>
2021-02-04 21:49:36 +0700

    separate layout

---
+++ src/layouts/BasicLayout/BasicLayout.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { Layout } from 'antd';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import NavBreadcrumb from '../../components/Navigation/NavBreadcrumb/NavBreadcrumb';
+
+const { Header, Content, Footer } = Layout;
+
+export default class BasicLayout extends Component {
+  static propTypes = {
+    prop: PropTypes
+  };
+
+  
+  render() {
+
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <SideDrawer />
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <NavBreadcrumb />
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              Bill is a cat.
+              {this.props.children}
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Presence ©2021 Created by HCMIU</Footer>
+        </Layout>
+      </Layout>
+    )
+  }
+}
